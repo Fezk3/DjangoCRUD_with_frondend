b46b02714efe83613e92bf4e500c29dd9d8ac216
Victor Segura <seguravictor57@gmail.com>
2023-02-02 18:06:09 -0600

    function views and urls to use them

--- students/urls.py
+++ students/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:student_id>', views.student_details, name='student_details'),
+    path('delete/<int:student_id>', views.student_delete, name='student_delete'),
+    path('add', views.add_student, name='add_student'),
+    path('edit/<int:student_id>', views.edit_student, name='edit_student'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,8 +1,44 @@
 from django.shortcuts import render
 from .models import Students
+from django.http import HttpResponseRedirect
+from .forms import StudentForm
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'students/index.html')
+def index(request): return render(request, 'students/index.html',
+                                  {'students': Students.objects.all()})
+
+
+def student_details(request, student_id):
+    student = Students.objects.get(pk=student_id)
+    return render(request, 'students/studenDetail.html', {'student': student})
+
+
+def student_delete(request, student_id):
+    student = Students.objects.get(pk=student_id)
+    student.delete()
+    return render(request, 'students/index.html', {'students': Students.objects.all()})
+
+
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'students/index.html', {'students': Students.objects.all()})
+    else:
+        form = StudentForm()
+    return render(request, 'students/add.html', {'form': form})
+
+
+def edit_student(request, student_id):
+    student = Students.objects.get(pk=student_id)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return render(request, 'students/index.html', {'students': Students.objects.all()})
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'students/edit.html', {'form': form})
