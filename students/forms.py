39ec4184f07a108ab7f4f272bc31ef52b78a6a54
Victor Segura <seguravictor57@gmail.com>
2023-02-02 18:04:52 -0600

    StudentForm with django form class

---
+++ students/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Students
+
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Students
+        fields = '__all__'
+        labels = {
+            'student_number': 'Student Number',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'field_of_study': 'Field of Study',
+            'gpa': 'GPA',
+        }
+
+        # This is the way to add classes to the form fields
+        widgets = {
+            'student_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'field_of_study': forms.TextInput(attrs={'class': 'form-control'}),
+            'gpa': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
